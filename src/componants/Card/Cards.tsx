a141fbf3bccc3402b7d09413535e7039cc7520ff
Trouble-Fete <neyroud.thomas@gmail.com>
2025-02-06 22:39:53 +0100

    debut css galery

--- src/componants/Card/Cards.tsx
+++ src/componants/Card/Cards.tsx
@@ -33,34 +33,36 @@ function emplois({ data }: JobsArray) {
 	return (
 		<>
 			<div className="containerCard">
-				<div className="partieHaute">
-					<img className="categorie" src={data.image} alt="coucou" />
+				<img className="categorie" src={data.image} alt="coucou" />
+
+				<div className="infoCard">
 					<h2>{data.nom_du_poste}</h2>
-				</div>
-				<p>{data.ville}</p>
-				<div>
-					<h3>Date du début:</h3>
-					<p>{data.date_debut}</p>
-				</div>
-				<div>
-					<h3>Durée de la mission:</h3>
-					<p>{data.duree}</p>
-				</div>
-				<div>
-					<h3>Description de la mission:</h3>
-					<p>{data.description_du_poste}</p>
-				</div>
-				<div>
-					<h3>Qualifications requises:</h3>
-					<p>{data.qualification_requise}</p>
-				</div>
-				<div>
-					<h3>Categorie:</h3>
-					<p>{data.categorie}</p>
-				</div>
-				<div>
-					<h3>Salaire:</h3>
-					<p>{data.salaire}</p>
+
+					<p>{data.ville}</p>
+					<div>
+						<h3>Date du début:</h3>
+						<p>{data.date_debut}</p>
+					</div>
+					<div>
+						<h3>Durée de la mission:</h3>
+						<p>{data.duree}</p>
+					</div>
+					<div>
+						<h3>Description de la mission:</h3>
+						<p>{data.description_du_poste}</p>
+					</div>
+					<div>
+						<h3>Qualifications requises:</h3>
+						<p>{data.qualification_requise}</p>
+					</div>
+					<div>
+						<h3>Categorie:</h3>
+						<p>{data.categorie}</p>
+					</div>
+					<div>
+						<h3>Salaire:</h3>
+						<p>{data.salaire}</p>
+					</div>
 				</div>
 			</div>
 		</>
