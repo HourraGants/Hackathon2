113be65f9ed0ee748bb12f07e80be94bb2f1512b
unknown <arthur.saussereau@hotmail.fr>
2025-02-06 21:03:14 +0100

    created the navbar and added the css

--- src/componants/Navbar/Navbar.tsx
+++ src/componants/Navbar/Navbar.tsx
@@ -1,9 +1,28 @@
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+	const location = useLocation();
+
 	return (
 		<>
-			<div className="navbar">je suis dans navbar</div>
+			<div className="navbar-container">
+				<div className="navbar">
+					<Link to="/">
+          <img src="../src/assets/logo.png" alt="Logo" title="Logo de la page" />
+          </Link>
+					<div>
+						<ul>
+							<li className={location.pathname === '/' ? 'active' : ''}>
+							<Link to="/">Home</Link>
+							</li>
+							<li className={location.pathname === '/galery' ? 'active' : ''}>
+							<Link to="/galery">Jobs</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
