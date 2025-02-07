e30bae9b9cbbb35e2887f3166d21b8a0b2cfa8ef
Trouble-Fete <neyroud.thomas@gmail.com>
2025-02-07 12:15:20 +0100

    css cards

--- src/componants/Card/Cards.tsx
+++ src/componants/Card/Cards.tsx
@@ -41,30 +41,30 @@ function emplois({ data }: JobsArray) {
 
 						<div>
 							<h3>Date du début:</h3>
-							<p>{data.date_debut}</p>
+							<p className="displayNoneCard">{data.date_debut}</p>
 						</div>
 						<div>
 							<h3>Durée de la mission:</h3>
-							<p>{data.duree}</p>
+							<p className="displayNoneCard">{data.duree}</p>
 						</div>
 						<div>
 							<h3>Description de la mission:</h3>
-							<p>{data.description_du_poste}</p>
+							<p className="displayNoneCard">{data.description_du_poste}</p>
 						</div>
 						<div>
 							<h3>Qualifications requises:</h3>
-							<p>{data.qualification_requise}</p>
+							<p className="displayNoneCard">{data.qualification_requise}</p>
 						</div>
 						<div>
 							<h3>Categorie:</h3>
-							<p>{data.categorie}</p>
+							<p className="displayNoneCard">{data.categorie}</p>
 						</div>
 					</div>
 					<div className="basCard">
 						<button className="boutonGauche" type="button">
 							{data.ville}
 						</button>
-						<button className="boutonGauche" type="button">
+						<button className="boutonthune" type="button">
 							{data.salaire}
 						</button>
 						<button className="linkBouton" type="button">
